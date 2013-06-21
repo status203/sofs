cc9bdfac0b452d4546d7ecb7273aad21dc5f695c
Adam Rulli-Gibbs <git@status203.com>
2013-06-21 14:08:29 +0100

    working shortest and longest lengths

--- src/sofs/core.clj
+++ src/sofs/core.clj
@@ -1,9 +1,38 @@
-(ns sofs.core)
+(ns sofs.core
+  (:require [clojure.core.reducers :as r]))
 
 (defn longest-count
-  "Takes a grid vector consisting of row vectors, and returns the
-  length of the longest row"
-  [grid]
-  (reduce #(max % (count %2))
-          0
-          grid))
+  "Takes a sequence of sequences and returns the length of the longest
+  inner sequence. Returns nil if s is empty"
+  [s]
+  (if (seq s)
+      (reduce max (map count s))
+      nil))
+
+(defn shortest-count
+  "Takes a sequence of sequences and returns the length of the longest
+  inner sequence. Returns nil if s is empty"
+  [s]
+  (if (seq s)
+   (reduce min (map count s))
+   nil))
+
+(defn longest
+  "Takes a sequence of sequences and returns the longest inner sequence.
+  Returns nil if sequence is empty."
+  [s]
+  (if (seq s)
+    (reduce #(if (> (count %2) (count %))
+               %2
+               %))
+    nil))
+
+(defn shortest
+  "Takes a sequence of sequences and returns the longest inner sequence.
+  Returns nil if sequence is empty."
+  [s]
+  (if (seq s)
+    (reduce #(if (< (count %2) (count %))
+               %2
+               %))
+    nil))
--- test/sofs/core_test.clj
+++ test/sofs/core_test.clj
@@ -8,8 +8,42 @@
         (longest-count ?grid) => ?length)
 
  ?grid               ?length
- []                  0
+ []                  nil
  [[1]]               1
  [[1] [2]]           1
  [[1 2] [3]]         2
  [[1] [3 4]]         2)
+
+(tabular
+ (facts "about finding the length of the shortest row"
+        (shortest-count ?grid) => ?length)
+
+ ?grid               ?length
+ []                  nil
+ [[1]]               1
+ [[1] [2]]           1
+ [[1 2] [3]]         1
+ [[1] [3 4]]         1)
+
+(tabular
+ (facts "about finding the longest row"
+        (longest ?grid) => ?length)
+
+ ?grid               ?length
+ []                  nil
+ [[1]]               [1]
+ [[1] [2]]           [1]
+ [[1 2] [3]]         [1 2]
+ [[1] [3 4]]         [3 4])
+
+(tabular
+ (facts "about finding the length of the longest row"
+        (shortest-count ?grid) => ?length)
+
+ ?grid               ?length
+ []                  nil
+ [[1]]               1
+ [[1] [2]]           1
+ [[1 2] [3]]         1
+ [[1] [3 4]]         1)
+
